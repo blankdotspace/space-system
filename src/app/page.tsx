561bfcd230309cbe2dabf596cd967edcb88bf102
Jesse Paterson <jesse.r.pat@gmail.com>
2025-12-02 16:38:51 -0600

    handling build time errors better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { loadSystemConfig } from "@/config";
 // Force dynamic rendering - config loading requires request context
 export const dynamic = 'force-dynamic';
 
+// Force dynamic rendering - config loading requires request context
+export const dynamic = 'force-dynamic';
+
 export default async function RootRedirect() {
   const config = await loadSystemConfig();
   
