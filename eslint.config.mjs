5ad92d625abe4301be3823ee44b42f0f165d8ee4
Jesse Paterson <jesse.r.pat@gmail.com>
2026-01-21 18:20:23 -0600

    Fix ESLint configuration for ESLint 9
    - Remove .eslintrc.json (old format conflicts with ESLint 9)
    - Remove plugin:@next/next/recommended from extends (causes 'name' property error)
    - Remove unused Next.js plugin rule reference
    - ESLint 9 uses flat config format (eslint.config.mjs)

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/node_modules/", "**/dist/", "**/.next/", "src/assets"]), {
+    extends: [
+        ...compat.extends("eslint:recommended"),
+        ...compat.extends("plugin:@typescript-eslint/recommended"),
+        ...compat.extends("plugin:react/recommended"),
+        ...compat.extends("prettier")
+    ],
+
+    plugins: {
+        "@stylistic": stylistic,
+        "@typescript-eslint": typescriptEslint,
+        react,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            varsIgnorePattern: "^_",
+            args: "none",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "off",
+        "react/prop-types": "off",
+
+        "@stylistic/max-len": ["error", {
+            code: 120,
+            ignoreUrls: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+        }],
+
+    },
+}]);
