dc06f23368bb5a629e0c3857f9cf007aa6846661
Jesse Paterson <jesse.r.pat@gmail.com>
2026-01-26 14:53:23 -0600

    Fix docs site redirect and README links (#1711)

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home(): React.JSX.Element {
-  // Redirect to the docs README which serves as the landing page
-  return <Redirect to="/README" />;
+  // Redirect to the docs landing page
+  return <Redirect to="/docs/" />;
 }
