6d63b934651586021e3a8c7fb92c8b50a74e6fce
Jhonattan2121 <devferri22@gmail.com>
2026-02-01 14:37:27 -0300

    fix: use inferred FID for editability without signer

--- src/common/data/stores/app/space/spaceStore.ts
+++ src/common/data/stores/app/space/spaceStore.ts
@@ -1027,6 +1027,7 @@ export const createSpaceStoreFunc = (
       const { data } = await axiosBackend.post<RegisterNewSpaceResponse>(
         "/api/space/registry",
         registration,
+        { timeout: 30000 },
       );
       const newSpaceId = data.value!.spaceId;
       
